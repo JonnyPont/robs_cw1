56551bcf22fe6b9804d0a0bc47368659b9328f21
Jon Pont <jp15101@bristol.ac.uk>
2018-11-10 17:20:43 +0000

    Application of intersection applied and Gus assistance given regarding localise

--- aStarSearch.m
+++ aStarSearch.m
@@ -7,28 +7,26 @@ open = start;
 
 map=[0,0;60,0;60,45;45,45;45,59;106,59;106,105;0,105];  %default map
 
-
-%Makes a kind of iterable map of the world. I think this is a map of points
-%rather than a map of squares/grids. I think I need it to be squares/grids
-%to be able to get anywhere as the robot will not move in exact integer
-%increments.
-sizes = max(map);
-mapGrid = zeros(sizes(1)+1,sizes(2)+1); %overcome 0 indexing
+mapGrid = zeros(10,10); % zeros(sizes(1)+1,sizes(2)+1); %overcome 0 indexing
+sizes = size(mapGrid);
 size(mapGrid)
 % gCostsMap(start(1)+1,start(2)+1) = 0; %set start cost to zero 
 
-
-% initialise g(x) and h(x) based on euclidean distances
-gCostsMap = zeros(sizes(1)+1,sizes(2)+1); %make a cost matrix - overcoming 1 indexing
+% initialise g(x) - inf and h(x) - based on euclidean distances
+gCostsMap = zeros(sizes(1)+1,sizes(2)+1)+Inf; %make a cost matrix - overcoming 1 indexing
+gCostsMap(start(1)+1,start(2)+1) = 0; %set start cost to zero 
 hCostsMap = zeros(sizes(1)+1,sizes(2)+1);
+
 for i=1:sizes(1)+1
     for j=1:sizes(2)+1
-        gCostsMap(i,j) = distance([i j],start);
         hCostsMap(i,j) = distance([i j],goal);
     end
 end    
 
 fCostsMap = gCostsMap + hCostsMap; % Which node is explored next completely depends on the fCostsMap. I will make sure this happens after ensuring heuristics get included and added in the script.
+
+% Determine what points are inside and outside of the map - might not be
+% needed
 for i=1:sizes(1)
     for j=1:sizes(2)
         if inpolygon(i,j,map(:,1),map(:,2)) == 1 %This is equivalent to botSim.pointInsideMap(mapGrid(i,j)) == 1 
@@ -43,17 +41,47 @@ end
 while length(open) ~= 0
    %open all nodes in open storing the cost g(x) to get to them.
    openSize = size(open); %Get list length of open
-   currentNode = open(1,:); % Get two corresponding points - slightly concerned about this method open(1+openSize(1))
+   currentNode = open(1,:); % Get two corresponding points
    currentNodeG = gCostsMap(currentNode(1)+1,currentNode(2)+1);
-   for i = 1:openSize(1)
-       testNode = open(i,:); 
-       testNodeG = gCostsMap(testNode(1)+1,testNode(2)+1);
-       if testNodeG <= currentNodeG
-           currentNode = testNode;
-           currentNodeLoc = i;
-           currentNodeG = testNodeG;
-       end
+   
+   % use intersection code to find visible nodes.
+   neighbourList = [];
+   for i = 1:sizes(1)+1
+       for j = 1:sizes(2)+1
+           check = 0;
+           neighbourTemp = [i,j]*9;
+           for k = 1:length(map)
+               if k <length(map)
+%                    boundingLine = [map(k,:),map(k+1,:)];
+                   x = intersection([currentNode neighbourTemp],[map(k,:),map(k+1,:)]);
+               elseif k == length(map)
+%                    boundingLine = [map(k,:),map(1,:)];
+                   x = intersection([currentNode neighbourTemp],[map(k,:),map(1,:)]);
+               end 
+               if ~isnan(x)
+                   check = 1;
+               end
+           end
+           if check == 0
+               neighbourList(end+1,:) = neighbourTemp;
+               fprintf('Add node')
+           end
+       end    
    end
+   
+   neighbourList
+   %This neighbour list is good and correct. Need to go on to find g scores
+   %of each member next!
+   
+%    for i = 1:openSize(1)
+%        testNode = open(i,:); 
+%        testNodeG = gCostsMap(testNode(1)+1,testNode(2)+1);
+%        if testNodeG <= currentNodeG
+%            currentNode = testNode;
+%            currentNodeLoc = i;
+%            currentNodeG = testNodeG;
+%        end
+%    end
    
    %At this point, the currentNode should be the node with the lowest f(x)
    %score
@@ -67,24 +95,25 @@ while length(open) ~= 0
    open(currentNodeLoc,:) = [];
    closed(end+1,:) = [currentNode(1) currentNode(2)];
    
-   %This loop should run through and find all neighbours of the currentNode
-   %ready to be tested and update the f(x) values of all these new nodes.
-   neighbourList = [];
-   for i = -1:1:1
-       for j = -1:1:1
-           neighbour = [currentNode(1)+i,currentNode(2)+j];
-           if inpolygon(neighbour(1),neighbour(2),map(:,1),map(:,2)) == 1 %no need for +1 here due to both being pre-calibrated
-               neighbourList = [neighbourList;neighbour];
-               %Find G of neighbour
-               neighbourG = currentNodeG + norm([[0 0] [i j]]); %gcost of node + distance to neighbour
-               %Update the neighbour's gCost matrix value IF this path to the specific node is shorted.
-               if neighbourG < gCostsMap(neighbour(1)+1,neighbour(2)+1)
-                   gCostsMap(neighbour(1)+1,neighbour(2)+1) = neighbourG;
-               end
-           end
-       end
-   end
-   
+%    %This loop should run through and find all neighbours of the currentNode
+%    %ready to be tested and update the f(x) values of all these new nodes.
+%    %I need to add a check of if the node is in the map or not
+%    neighbourList = [];
+%    for i = -1:1:1
+%        for j = -1:1:1
+%            neighbour = [currentNode(1)+i,currentNode(2)+j];
+%            if inpolygon(neighbour(1),neighbour(2),map(:,1),map(:,2)) == 1 %no need for +1 here due to both being pre-calibrated
+%                neighbourList = [neighbourList;neighbour];
+%                %Find G of neighbour
+%                neighbourG = currentNodeG + norm([[0 0] [i j]]); %gcost of node + distance to neighbour
+%                %Update the neighbour's gCost matrix value IF this path to the specific node is shorted.
+%                if neighbourG < gCostsMap(neighbour(1)+1,neighbour(2)+1)
+%                    gCostsMap(neighbour(1)+1,neighbour(2)+1) = neighbourG;
+%                end
+%            end
+%        end
+%    end
+%    
    %Check to see if a neighbour has already been explored. - hasn't been
    %tested for functionality
    neighbourListLength = size(neighbourList);
@@ -109,7 +138,7 @@ while length(open) ~= 0
 %         heuristics(i) = distance(neighbourList(i,:),goal);
 %    end
 
-fCostsMap
+fCostsMap;
 
 %    for 'each neighbour of currentNode(in all directions)'
 %         if neighbour 'in closed'
--- intersection.m
+++ intersection.m
@@ -10,7 +10,7 @@ function [ crossingPoint] = intersection(infVec1,line2)
 %   crossingpoint = [1.5 3];
 ua = ((line2(:,3)-line2(:,1)).*(infVec1(:,2)-line2(:,2))-(line2(:,4)-line2(:,2)).*(infVec1(:,1)-line2(:,1)))./(((line2(:,4)-line2(:,2)).*(infVec1(:,3)-infVec1(:,1))-(line2(:,3)-line2(:,1)).*(infVec1(:,4)-infVec1(:,2))));
 ub = ((infVec1(:,3)-infVec1(:,1)).*(infVec1(:,2)-line2(:,2))-(infVec1(:,4)-infVec1(:,2)).*(infVec1(:,1)-line2(:,1)))./(((line2(:,4)-line2(:,2)).*(infVec1(:,3)-infVec1(:,1))-(line2(:,3)-line2(:,1)).*(infVec1(:,4)-infVec1(:,2))));
-% filter = ub >= 0 & ub <= 1 & ua >= 0 & ua <= 1
+% filter = ub >= 0 & ub <= 1 & ua >= 0 & ua <= 1;
 filter = ub >= 0 & ub <= 1 & ua>=0; %
 crossingPoint =[infVec1(:,1)+ua.*(infVec1(:,3)-infVec1(:,1)) infVec1(:,2)+ua.*(infVec1(:,4)-infVec1(:,2))];
 for i = 1:length(filter)
