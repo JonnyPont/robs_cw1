bb934eac17c1b62f80796147e448db12d6f6c207
Jon Pont <jp15101@bristol.ac.uk>
2018-11-12 19:23:15 +0000

    Resolution solution to null routes solved

--- aStarSearch.m
+++ aStarSearch.m
@@ -21,10 +21,7 @@ startCostsMap = zeros(sizes(1)+1,sizes(2)+1)+Inf; %make a cost matrix - overcomi
 parents = cell(sizes(1)+1,sizes(2)+1);
 hCostsMap = zeros(sizes(1)+1,sizes(2)+1);
 
-
 %Find nearest node to start position.
-
-
 for i=1:resolution:sizes(1)+1
     for j=1:resolution:sizes(2)+1
         hCostsMap(i,j) = distance([i j],goal);
@@ -34,18 +31,19 @@ for i=1:resolution:sizes(1)+1
 end
 
 %I need to make sure that this only ever gives 1 value. Even when
-%equidistant
+%equidistant - equidistance isn't the issue
 [minVal,minValIndexSquashed] = min(reshape(startCostsMap, numel(startCostsMap),1));
-[indexI,indexJ] = ind2sub(size(startCostsMap),minValIndexSquashed)
+[indexI,indexJ] = ind2sub(size(startCostsMap),minValIndexSquashed);
 gCostsMap(indexI+1,indexJ+1) = 0; %set start cost to zero
 firstNode = [indexI indexJ];
-
 
 closed = [];
 open = firstNode;
 completable = 1;
+iteration = 0;
 while isempty(open) == 0 && completable == 1
     %open all nodes in open storing the cost g(x) to get to them.
+    iteration = iteration + 1;
     openSize = size(open); %Get list length of open
     currentNode = open(1,:); % Get two corresponding points
     currentNodeG = gCostsMap(currentNode(1)+1,currentNode(2)+1);
@@ -54,16 +52,13 @@ while isempty(open) == 0 && completable == 1
     % use intersection code to find visible nodes.
     neighbourList = [];
     lengthClosed = size(closed);
-    
+
     for i = 1:resolution:sizes(1)+1
         for j = 1:resolution:sizes(2)+1
             check = 0;
             alreadyExplored = 0;
             neighbourTemp = [i,j];
-            %I should make an iteration where the first loop, the code
-            %should check if the goal destination is visible and if it is,
-            %then move straight to it. ie. as soon as the goal is in sight,
-            %go for it.
+            
             for k = 1:length(map)
                 if k <length(map)
                     x = intersection1([currentNode goal],[map(k,:),map(k+1,:)]);
@@ -105,8 +100,11 @@ while isempty(open) == 0 && completable == 1
             end
         end
     end
-    
-    neighbourList;
+%     if iteration == 1
+%         for i = 1:length(neighbourList)
+%             plot(neighbourList(i,1),neighbourList(i,2),'g.')
+%         end
+%     end
     %This neighbour list is good and correct. Need to go on to find g scores
     %of each member next!
     
@@ -115,11 +113,8 @@ while isempty(open) == 0 && completable == 1
     for i = 1:lengthNeighbourList(1)
         dist = distance(currentNode,neighbourList(i,:));
         newGScore = dist + currentNodeG;
-        gCostsMap(neighbourList(i,1),neighbourList(i,2));
         if newGScore < gCostsMap(neighbourList(i,1)+1,neighbourList(i,2)+1)
             gCostsMap(neighbourList(i,1)+1,neighbourList(i,2)+1) = newGScore;
-            neighbourList(i,1);
-            neighbourList(i,2);
             parents{neighbourList(i,1)+1,neighbourList(i,2)+1} = currentNode; 
             tempFScores(i) = newGScore + hCostsMap(neighbourList(i,1)+1,neighbourList(i,2)+1);
         else
@@ -127,9 +122,6 @@ while isempty(open) == 0 && completable == 1
         end
     end
         
-    tempFScores;
-
-    
     if goalVisible == 1
         fprintf('Goal found')
         nodePathTrack = open(1,:);
@@ -158,62 +150,12 @@ while isempty(open) == 0 && completable == 1
 % same score. Better to move big.
     nextNodeIndex = find(tempFScores==min(tempFScores),1,'last');
     open = []; %only one node stored so reset open
-    open(end+1,:) = [neighbourList(nextNodeIndex,1) neighbourList(nextNodeIndex,2)];
-    closed(end+1,:) = [currentNode(1) currentNode(2)]; %add explored node to closed
-%     if ~isempty(nextNodeIndex)
-% %         fprintf('problemo')
-% 
-%     end
-    
-%    for i = 1:openSize(1)
-%        testNode = open(i,:); 
-%        testNodeG = gCostsMap(testNode(1)+1,testNode(2)+1);
-%        if testNodeG <= currentNodeG
-%            currentNode = testNode;
-%            currentNodeLoc = i;
-%            currentNodeG = testNodeG;
-%        end
-%    end
-   
-   %At this point, the currentNode should be the node with the lowest f(x)
-   %score
-
-   
-%    %This loop should run through and find all neighbours of the currentNode
-%    %ready to be tested and update the f(x) values of all these new nodes.
-%    %I need to add a check of if the node is in the map or not
-%    neighbourList = [];
-%    for i = -1:1:1
-%        for j = -1:1:1
-%            neighbour = [currentNode(1)+i,currentNode(2)+j];
-%            if inpolygon(neighbour(1),neighbour(2),map(:,1),map(:,2)) == 1 %no need for +1 here due to both being pre-calibrated
-%                neighbourList = [neighbourList;neighbour];
-%                %Find G of neighbour
-%                neighbourG = currentNodeG + norm([[0 0] [i j]]); %gcost of node + distance to neighbour
-%                %Update the neighbour's gCost matrix value IF this path to the specific node is shorted.
-%                if neighbourG < gCostsMap(neighbour(1)+1,neighbour(2)+1)
-%                    gCostsMap(neighbour(1)+1,neighbour(2)+1) = neighbourG;
-%                end
-%            end
-%        end
-%    end
-%    
-%    %Check to see if a neighbour has already been explored. - hasn't been
-%    %tested for functionality
-%    neighbourListLength = size(neighbourList);
-%    lengthClosed = size(closed);
-%    for i = 1:neighbourListLength(1)
-%        alreadyExplored = 0;
-%        for j = 1:lengthClosed(1)
-%            if neighbourList(i,:) == closed(j,:)
-%                 alreadyExplored = 1;
-%            end
-%        end
-%        if alreadyExplored == 0
-%             open(end+1,:) = neighbourList(i);
-%        end
-%    end    
-
+    if isempty(nextNodeIndex)
+        path = [];
+    else    
+        open(end+1,:) = [neighbourList(nextNodeIndex,1) neighbourList(nextNodeIndex,2)];
+        closed(end+1,:) = [currentNode(1) currentNode(2)]; %add explored node to closed
+    end
 end
 
 end
--- callAStar.m
+++ callAStar.m
@@ -4,26 +4,36 @@ clear;      %clears workspace
 axis equal; %keeps the x and y scale the same
 %default map
 %map=[0,0;60,0;60,45;45,45;45,59;106,59;106,105;0,105];  
-% %Gus map of fun
+%Gus map of fun
 map=[0,0;0,50;75,50;75,65;10,65;10,85;35,85;35,125;10,125;10,145;75,145;75,85;110,85;110,155;125,155;125,40;150,40;150,15;100,15;100,0;80,0;80,30;40,30;40,0];
 % A symmetric map
 
-% start = [floor(rand*100)+1 floor(rand*100)+1]
-% % goal = [floor(rand*100)+1 floor(rand*100)+1]
-% goal = [60 32]
-resolution = 12;
 robot = BotSim(map,[0.01,0.005,0]);  %sets up a botSim object a map, and debug mode on.
 
+%Random Positions
 start = robot.getRndPtInMap(10)
 goal = robot.getRndPtInMap(10)
 
+%Error positions
+% start = [45.18 124.34]
+% goal = [115 73]
+% 
+% start = [10.25 33.61]
+% goal = [47.15 82.74]
 
 robot.drawMap();
 plot(goal(1),goal(2),'r*');
 plot(start(1),start(2),'g*');
 drawnow;
+
+resolution = 12;
+
 path = aStarSearch(start,goal,map,resolution);
 
+while isempty(path)
+    resolution = resolution - 2;
+    path = aStarSearch(start,goal,map,resolution);
+end    
 
 pathLength = size(path);
 for i = 1:(pathLength(1)-1)
